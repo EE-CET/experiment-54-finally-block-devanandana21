81206f7235a486fcbe24a5586f6e5d6cdd79b08d
devanandana21 <tve24el018@cet.ac.in>
2026-02-13 09:54:02 +0530

    Update FinallyDemo.java

--- src/FinallyDemo.java
+++ src/FinallyDemo.java
@@ -1,17 +1,21 @@
 import java.util.Scanner;
 
 public class FinallyDemo {
-    
-        // TODO: Read a and b
-        
-        // TODO: try block
-        // Perform division
-        // Print "Result: " + result
-        
-        // TODO: catch ArithmeticException e
-        // Print "Error: " + e.getMessage()
-        
-        // TODO: finally block
-        // Print "Finally block executed"
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter first integer: ");
+        int a=sc.nextInt();
+        System.out.println("Enter second integer: ");
+        int b=sc.nextInt();
+        try{
+            int result=a/b;
+            System.out.println("Result:" + result);
+            }catch (ArithmeticException e){
+                System.out.println("Divide by zero error");
+            }
+        finally {
+            System.out.println("Finally block executed");
+        }
+
     }
 }
